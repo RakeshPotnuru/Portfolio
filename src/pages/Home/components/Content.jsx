d34cbac3af68e1d8deeb4fbe65e89be90c7d8295
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2023-01-24 16:45:34 +0530

    Update data
    Signed-off-by: RakeshPotnuru <rakeshpotnuru321@gmail.com>

--- src/pages/Home/components/Content.jsx
+++ src/pages/Home/components/Content.jsx
@@ -40,7 +40,7 @@ const Content = (props) => {
             <Col className="content__container__details" sm={12} md={6}>
               <div>
                 <h1>
-                  <AnimatedCounter end={50} duration={1} />
+                  <AnimatedCounter end={6} duration={1} />
                   <span style={{ color: '#8ED2A9' }}>+</span>
                 </h1>
                 <h6>Completed projects</h6>
